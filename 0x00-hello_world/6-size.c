8dee001977ee5b0f3ed697d4f106e4b44c8bf57b
0xTariq-dev <tariqmohsen222@gmail.com>
2023-06-15 13:11:34 -0700

    Added the print values program script

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,11 +12,11 @@
  */
 	int main(void)
 	{
-		printf("Size of a cahr: %d bute(s)\n", sizeof(char));
-		printf("Size of an int: %d bute(s)\n", sizeof(int));
-		printf("Size of a long int: %d bute(s)\n", sizeof(long int));
-		printf("Size of a long long int: %d bute(s)\n", sizeof(long long int));
-		printf("Size of a float: %d bute(s)\n", sizeof(float));
+		printf("Size of a cahr: %d byte(s)\n", sizeof(char));
+		printf("Size of an int: %d byte(s)\n", sizeof(int));
+		printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+		printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+		printf("Size of a float: %d byte(s)\n", sizeof(float));
 
 		return (0);
 	}
