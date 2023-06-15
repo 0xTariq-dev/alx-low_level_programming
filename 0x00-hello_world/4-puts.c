4238812e1ac6afc00d50345988c4957fc49b6756
0xTariq-dev <tariqmohsen222@gmail.com>
2023-06-15 12:41:20 -0700

    Added the C puts program script

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,14 +5,14 @@
 
 	# include <stdio.h>
 /*
- * Print "programming is like building a multilingual puzzle,
- * 	followed by a new line.
+ * main-Print "programming is like building a multilingual puzzle,
+ *	followed by a new line.
  *
  * and returuns 0
  */
 	int main(void)
 	{
 		puts("\"Programming is like building a multilingual puzzle");
-		
-		return(0);
+
+		return (0);
 	}
