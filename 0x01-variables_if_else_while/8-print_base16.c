c6a9810d5ea6a6e0e33a4175283eac88b651c366
0xTariq-dev <tariqmohsen222@gmail.com>
2023-06-16 18:52:34 -0700

    Added the program Print all base 16  numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/*
+ * Author: 0xTariq-dev
+ * program name: print base 16 numbers
+ */
+
+/**
+ * main - print the numbers of base 16 in lowercase.
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+
+	int number;
+	char letter;
+
+	for (number = 0; number < 10; number++)
+		putchar((number % 10) + '0');
+
+	for (letter = 'a'; letter <= 'f'; letter++)
+		putchar(letter);
+	
+	putchar('\n');
+
+	return (0);
+}
