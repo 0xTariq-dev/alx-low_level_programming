c6b77623efc64618053dc6fad607bb4400ae6879
0xTariq-dev <tariqmohsen222@gmail.com>
2023-06-16 18:16:07 -0700

    Added the program Print alphabets in lower and Upper case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/*
+ * Author: 0xTariq-dev
+ * program name: Last Digit
+ * including the standard and time library
+ */
+
+/**
+ * main - print the last digit of random number
+ *	and whether it is greater than 5, less than 6, or 0
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+	
+	for (letter = 'A'; letter <= 'Z'; letter++)
+                putchar(letter);
+	
+	putchar('\n');
+
+	return (0);
+}
