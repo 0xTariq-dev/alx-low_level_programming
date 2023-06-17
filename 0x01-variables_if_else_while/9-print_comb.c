657317345f613f3915f7c7c997909f0d861004c4
0xTariq-dev <tariqmohsen222@gmail.com>
2023-06-16 19:08:00 -0700

    Added the program Print all possible combination of numbers followed by a commax

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -18,9 +18,15 @@ int main(void)
 	int number;
 
 	for (number = '0'; number <= '9'; number++)
-		putchar(number, ',', ' ');
+	{
+		putchar((number % 10)+ '0');
+		if (num ==9)
+			continue;
+
+		putchar(',');
+		putchar(' ');
 
 	putchar('\n');
-
+	}
 	return (0);
 }
