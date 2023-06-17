386ae0507d5fb9fba15a8d3b836682033fbbde81
0xTariq-dev <tariqmohsen222@gmail.com>
2023-06-16 19:02:25 -0700

    Added the program Print all possible combination of numbers followed by a commax

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/*
+ * Author: 0xTariq-dev
+ * program name: print all single digit numbers combination
+ */
+
+/**
+ * main - prints all possible combinations of single digit numbers
+ * in ascending order
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+
+	int number;
+
+	for (number = '0'; number <= '9'; number++)
+		putchar(number, ',', ' ');
+
+	putchar('\n');
+
+	return (0);
+}
