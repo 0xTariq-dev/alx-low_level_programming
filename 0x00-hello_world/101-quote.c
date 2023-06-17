8c400f4e45b39cd647ca4ed5c7233507b995a858
0xTariq-dev <tariqmohsen222@gmail.com>
2023-06-16 19:18:52 -0700

    Added the file 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,20 +1,21 @@
 /*
- * File: 5-printf.c
+ * File: 101-quote.c
  * Auth: 0xTariq-dev
  */
 
-	# include <stdio.h>
+# include <stdio.h>
+
 /**
  * main - Prints "and that piece of art is useful\" - Dora Korpar, 2015-10-19"
  *	followed by a new line, to standard error.
  *
  * Return: Always 1.
  */
-	int main(void)
-	{
-		write(2,
-			"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
-			,61);
+int main(void)
+{
+	write(2,
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+		,60);
 
-		return (1);
-	}
+	return (1);	
+}
