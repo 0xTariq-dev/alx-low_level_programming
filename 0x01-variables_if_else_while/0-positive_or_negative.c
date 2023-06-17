d53faad0fb5a8e56ff69ec33987583bd9476d862
0xTariq-dev <tariqmohsen222@gmail.com>
2023-06-16 17:43:30 -0700

    Added the program positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,12 +3,13 @@
 #include <stdio.h>
 /*
  * Author: 0xTariq-dev
- * program name: positive or nigative 
- * including the standard and time library 
+ * program name: positive or nigative
+ * including the standard and time library
  */
 
-/* 
- * main - print the value of n and identifies wether it's postitive, negative or zero
+/*
+ * main - print the value of n and identifies wether 
+ * it's postitive, negative or zero
  * 
  * Return: Always 0
  */
@@ -18,15 +19,15 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* 
+	/*
 	 * The program consists of three conditions
-	 * that evaluates the value of n if it's positive, negative or zero  
+	 * that evaluates the value of n if it's positive, negative or zero
 	 */
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
 	}
-	else if (n ==0)
+	else if (n == 0)
 	{
 		printf("%d is zero\n", n);
 	}
