680fc43f9d7b3e150afd0185b19277b36c48c3c9
0xTariq-dev <tariqmohsen222@gmail.com>
2023-06-16 18:42:17 -0700

    Added the program Print all numbers from 0 to 9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/*
+ * Author: 0xTariq-dev
+ * program name: print numbers
+ */
+
+/**
+ * main - print the numbers from 0 to 9 followed by a new line
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+
+	int number;
+
+	for (number = '0'; number <= '9'; number++)
+		putchar(number);
+
+	putchar('\n');
+
+	return (0);
+}
