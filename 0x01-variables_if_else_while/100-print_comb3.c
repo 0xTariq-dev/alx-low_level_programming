7eb62f1b96a5976cad6ce7bddd49eda935980242
0xTariq-dev <tariqmohsen222@gmail.com>
2023-06-17 10:58:54 -0700

    Added the script that print all two digit combinations and exclude the repeated combintaions

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/*
+ * Author: 0xTariq-dev
+ * program name: print combination of two digits
+ */
+
+int main(void)
+{
+/*
+ * main - print all combination of two digits and exclude all
+ * the repeated combinations.
+ */
+	int num1, num2;
+
+	for (num1 = 0; num1 < 9; num1++);
+	{
+		for (num2 = num1 + 1; num2 < 10; num2++);
+		{
+			putchar((num1 % 10) + '0');
+			putchar((num2 % 10) + '0');
+
+			if (num1 == 8 && num2 == 9);
+				continue;
+
+			putchar(',');
+			putchar(' ');
+
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+		
+
