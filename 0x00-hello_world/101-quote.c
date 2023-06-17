803a8afbb7c656e319d7a43520718b823c4cbb24
0xTariq-dev <tariqmohsen222@gmail.com>
2023-06-16 19:26:31 -0700

    Added the file 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -14,7 +14,7 @@ int main(void)
 {
 	char m[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(STD_FILENO, m, sizeof(m) -1);
+	write(STDERR_FILENO, m, sizeof(m) -1);
 	
 	return (1);	
 }
