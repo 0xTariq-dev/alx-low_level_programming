30af843d43e68935b24fc35df151bd9c3cf696f0
0xTariq-dev <tariqmohsen222@gmail.com>
2023-06-21 15:39:23 -0700

    Modified the file 100-times_table.c and used a different approach with two nested for loops and two integers values i and j and prints the result in %-4d format.

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,37 +13,19 @@
  */
 void print_times_table(int n)
 {
-	int num, multi, prod;
+	int i, j;
 
-	if (n >= 0 && n <= 15)
+	if (n < 0 || n > 15)
 	{
-		for (num = 0; num <= n; num++)
+		return (0);
+	}
+
+	for (i = 0; i <= 12; i++)
+	{
+		for (j = 0; j <= 12; j++)
 		{
-			_putchar('0');
-
-			for (multi = 0; multi <= n; multi++)
-			{
-				_putchar(',');
-				_putchar(' ');
-
-				prod = num * (multi + 1);
-
-				if (prod <= 99)
-					_putchar(' ');
-				if (prod <= 9)
-					_putchar(' ');
-				if (prod >= 100 && prod <= 999)
-				{
-					_putchar((prod / 100) + '0');
-					_putchar(((prod / 100)) % 10 + '0');
-				}
-				else if (prod <= 99 && prod >= 10)
-				{
-					_putchar((prod / 10) + '0');
-				}
-				_putchar((prod % 10) + '0');
-			}
-			_putchar('\n');
+			printf("%-4d, " i * J);
 		}
+		printf("\n");
 	}
 }
