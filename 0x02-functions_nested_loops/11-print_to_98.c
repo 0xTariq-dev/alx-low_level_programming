9cfdf441eaa9c7120eb52b46679a7f9c1c7cb330
0xTariq-dev <tariqmohsen222@gmail.com>
2023-06-20 21:30:16 -0700

    Modified the file 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,20 +1,18 @@
-/* Author: 0xTariq-dev
+/*
+ * Author: 0xTariq-dev
  * File: 11-print_to_98.c
  */
 
 #include "main.h"
 
 /**
- * add - Adds two integers.
- * @number1: The first integer value.
- * @number2: The second integer value.
+ * print_to_98 - prints the numbers till the number 98.
+ * @n: The value of integer to be printed.
  *
  * Return: The result of the addition
  */
 void print_to_98(int n)
 {
-	int n;
-
 	for (n < 0; n <= 98; n++)
 	{
 		printf("%d, ", n);
