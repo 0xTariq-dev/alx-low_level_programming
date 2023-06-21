8e925718fd12d05481f45d23be96687e1836f0ae
0xTariq-dev <tariqmohsen222@gmail.com>
2023-06-20 19:08:49 -0700

    added the fi1e 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include"main.h"
+
+/**
+ * _islower - pchecks if the letter is lowercase.
+ */
+
+int _islower(int c)
+{
+	int l;
+
+	if (l =  97, l <= 122)
+			_putchar('1');
+	else
+		_putchar('0');
+	}
+}
