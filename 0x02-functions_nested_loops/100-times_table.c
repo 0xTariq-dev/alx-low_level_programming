df8add46e0c6b9065d8579a74c798fc333846654
0xTariq-dev <tariqmohsen222@gmail.com>
2023-06-21 21:37:36 -0700

    refined the script of the file 101-natural.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,44 +1,40 @@
 #include "main.h"
 /**
- * print_times_table - prints the times table of the input starting
- *	with 0.
+ * print_times_table - prints the times table of the input starting with 0.
  * @n: The value of times table to be printed.
- *
  */
 void print_times_table(int n)
 {
-	int i, j, prod;
-	int max_digits = 0;
-	int digits = 0;
-	int spaces;
+	int i, j, p, s, d = 0, m_d = 0, f = i * 0, f_d = 0;
 
 	if (n < 0 || n > 15)
 	{
 		return;
 	}
-
-	prod = n * n;
-	while (prod > 0)
+	p = n * n;
+	while (p > 0)
 	{
-		max_digits++;
-		prod /= 10;
+		m_d++;
+		p /= 10;
 	}
-
 	for (i = 0; i <= n; i++)
 	{
+		while (f > 0)
+		{
+			f_d++;
+			f /= 10;
+		}
+		printf("%*d", f_d == 0 ? 1 : f_d, i * j);
 		for (j = 0; j <= n; j++)
 		{
 			prod = i * j;
-			while (prod > 0)
+			while (p > 0)
 			{
-				digits++;
-				prod /= 10;
+				d++;
+				p /= 10;
 			}
-			spaces = max_digits - digits;
-			printf("%*d", spaces + digits,
-					i * j == 0 ? 0 : i * j);
-
-
+			s = m_d - d;
+			printf("%*d", s + d, i * j);
 			if (j != n)
 			{
 			printf(", ");
