6e9cac49d2efbd4ca99fd7cc55779d10e27135c7
0xTariq-dev <tariqmohsen222@gmail.com>
2023-06-22 07:20:46 -0700

    Added the first file 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,10 @@
-/*
- * Author: 0xTariq-dev
- * File: 0-main.c
- */
-
 #include "main.h"
 
 /**
  * main - check the output of the function positive_or_negative.
  * Return: 0
  */
+
 int main(void)
 {
 	int i;
