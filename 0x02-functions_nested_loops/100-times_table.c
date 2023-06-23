aa998047d85f7f089e9eed9ad44f5e8ab19f5ca6
0xTariq-dev <tariqmohsen222@gmail.com>
2023-06-23 12:21:25 -0700

    Modified the file 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -25,7 +25,7 @@ void print_times_table(int n)
 				if (prod <= 9)
 					_putchar(' ');
 
-				if(prod >= 100)
+				if (prod >= 100)
 				{
 					_putchar((prod / 100) + '0');
 					_putchar(((prod / 10)) % 10 + '0');
@@ -36,6 +36,8 @@ void print_times_table(int n)
 				}
 				_putchar((prod % 10) + '0');
 			}
+			-putchar('\n');
 		}
+		-putchar('\n');
 	}
 }
