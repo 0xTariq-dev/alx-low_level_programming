850a20b7428d8894701d2a2ed0a77f80424855a9
0xTariq-dev <tariqmohsen222@gmail.com>
2023-06-22 19:51:06 -0700

    modified the main.h file

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -8,7 +8,7 @@
  * describtion: Header file contains declarations for functions
  * 		used in the 0x04-more_functions_nested_loops.
  */
-
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
