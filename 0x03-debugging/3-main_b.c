b9adf5e0ccdc49ceeb3e97f794d76adca6adf695
0xTariq-dev <tariqmohsen222@gmail.com>
2023-06-22 17:57:43 -0700

    Added the files 3-print_remaining_days.c, 3-convert_day.c and 3-main_b.c

--- 0x03-debugging/3-main_b.c
+++ 0x03-debugging/3-main_b.c
@@ -3,20 +3,20 @@
 
 /**
  * main - entry point
- * Resturn: 0
+ * Return: 0
  */
 
 int main(void)
 {
-	int month,day,year;
+	int month, day, year;
 
-	month = 4;
-	day = 01;
-	year = 1997;
+	month = 02;
+	day = 29;
+	year = 2000;
 
 	printf("Date: %02d/%02d/%04d\n", month, day, year);
 
-	day = convert_day(month,day);
+	day = convert_day(month, day);
 
 	print_remaining_days(month, day, year);
 
