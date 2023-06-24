567dd714530630489f39cc1d9195d767d84664a8
0xTariq-dev <tariqmohsen222@gmail.com>
2023-06-24 06:35:32 -0700

    fixed the source code of the executable 100-times_table.c to be less than 40 lines

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -16,30 +16,20 @@ void print_times_table(int n)
 				prod = num * multi;
 
 				if (multi == 0)
-				{
 					_putchar('0');
-				}
 				else
 				{
 					_putchar(',');
 					_putchar(' ');
 
 					if (prod >= 100)
-					{
 						_putchar((prod / 100) + '0');
-					}
 					else
-					{
 						_putchar(' ');
-					}
 					if (prod >= 10)
-					{
 						_putchar(((prod / 10)) % 10 + '0');
-					}
 					else
-					{
 						_putchar(' ');
-					}
 					_putchar((prod % 10) + '0');
 				}
 			}
