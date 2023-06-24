3361889637438cb19b05edd9f143efe78fa10377
0xTariq-dev <tariqmohsen222@gmail.com>
2023-06-24 07:34:51 -0700

    fixed the code of the file 103-fibonacci.c to print the sum not the actual numbers

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -13,19 +13,20 @@
  */
 int main(void)
 {
-	unsigned long int i = 0, x = 1, r = i + x;
+	unsigned long int i = 0, x = 1, r = i + x, sum = 0;
 
 	while (r <= 4000000)
 	{
 		if (r % 2 == 0 && r < 4000000)
 		{
-			printf("%ld", r);
-			printf(", ");
+			sum = sum + r;
 		}
 
 		i = x;
 		x = r;
 		r = i + x;
 	}
+	printf("%lu\n", sum);
+
 	return (0);
 }
