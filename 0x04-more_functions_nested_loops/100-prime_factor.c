62bb575ffd29c8c51f4e53ebb310529c5df6b1a4
0xTariq-dev <tariqmohsen222@gmail.com>
2023-06-25 20:00:39 -0700

    Modified the file 100-prime_numbers

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -4,21 +4,27 @@
  */
 
 #include <stdio.h>
+#include <math.h>
 
 /**
  * main - Finds and prints the prime factors of the number "612852475143".
  */
 int main(void)
 {
-	unsigned long int n = 612852475143;
-	unsigned long int p_f = 2;
+	unsigned long int n = 612852475143, p_f = 2;
 
-	while (n != 1)
+	while (p_f < (n / 2))
 	{
 		if (n % p_f == 0)
-			n = n / p_f;
-		else
-			p_f++;
+		{
+			n /= 2;
+			continue;
+		}
+		for (p_f = 3; p_f < (n / 2); p_f += 2)
+		{
+			if (n % p_f == 0)
+				n /= p_f;
+		}
 	}
 
 	printf("%lu\n", p_f);
