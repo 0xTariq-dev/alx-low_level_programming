0b4aa0e63ea3aea375d78c5032cc6f4f3ad37b8c
0xTariq-dev <tariqmohsen222@gmail.com>
2023-06-26 19:28:57 -0700

    Added the file 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+/*
+ * Author: 0xTariq-dev
+ * File: 5-rev_string.c
+ */
+
+#include "main.h"
+
+/**
+ * rev_string - Rverses thee order of a string.
+ * @n - The value
+ * Return: Always 0.
+ */
+void rev_string(char *s)
+{
+	char rev_s[10];
+	int rev = 0;
+
+	while (s[rev] != '\0')
+		rev_s[] = s[--rev];
+
+}
