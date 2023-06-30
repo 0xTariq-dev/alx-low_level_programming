0924936c3606a05cfd502b9d2902d162e9371530
0xTariq-dev <tariqmohsen222@gmail.com>
2023-06-29 17:39:59 -0700

    Added the files 0-strcat.c , 1-strncat.c and 2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,33 @@
+/*
+ * Author: 0xTariq-dev
+ * File: 1-strncat.c
+ */
+
+#include "main.h"
+
+/**
+ * *_strncat - Appends a number of characters from src
+ *	 string to the dest string,
+ *	 overwriting the null byte at the end of dest.
+ * @dest: The string to be appended.
+ * @src: The string to be copied from.
+ * @n: The number of bytes to be copied.
+ * Return: A pointer to the resulting string.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, x = 0, n;
+
+	while (dest[i] != '\0')
+		i++;
+
+	while (src[x] != '\0' && x < n)
+	{
+		dest[i] = src[x];
+		x++;
+		i++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
