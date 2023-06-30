50e241114b54f6e5f798a4c04c3543ceee520214
0xTariq-dev <tariqmohsen222@gmail.com>
2023-06-29 19:09:01 -0700

    Added the file 7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,31 @@
+/*
+ * Author: 0xTariq-dev
+ * File: 7-leeet.c
+ */
+
+#include "main.h"
+
+/**
+ * leet - Encodes a string in 1337.
+ * @s: The string to be encoded.
+ * Return: The resulting string.
+ */
+char *leet(char *s)
+{
+	int i, x;
+
+	char *a = "AaEeOoTtLl";
+	char *b = "4433007711";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (x = 0; x < 10; x++)
+		{
+			if (s[i] == a[x])
+			{
+					s[i] = b[x];
+			}
+		}
+	}
+	return (s);
+}
