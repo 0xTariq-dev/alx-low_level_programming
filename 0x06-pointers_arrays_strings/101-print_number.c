9d5775e34e851a42d3d34810b500b4fd970e15c0
0xTariq-dev <tariqmohsen222@gmail.com>
2023-06-30 10:02:33 -0700

    Added the file 101-print_number.c that print an integer

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,30 @@
+/*
+ * Author: 0xTariq-dev
+ * File: 101-print_number.c
+ */
+
+#include "main.h"
+
+/**
+ * print_number - Print an integer.
+ * @n: The integer to be printed.
+ */
+void print_number(int n)
+{
+	unsigned int x;
+
+	if (n < 0)
+	{
+		x = -n;
+		_putchar('-');
+	}
+	else
+	{
+		x = n;
+	}
+	if (x / 10)
+	{
+		print_number(x / 10);
+	}
+	_putchar((x % 10) + '0');
+}
