19acfb14e115f22f9848dfba596cb2960d839878
0xTariq-dev <tariqmohsen222@gmail.com>
2023-06-29 19:00:36 -0700

    Added the file 6-cap_string.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -6,30 +6,21 @@
 #include "main.h"
 
 /**
- * *cap_string - Capitalize the first letter of every word in string.
- * @s: The string to be modified.
- * Return: The resulting string.
+ * string_toupper - changes all lowercase letters of a string
+ * to uppercase
+ * @s: string to modify
+ *
+ * Return: the resulting string
  */
-char *cap_string(char *s)
+char *string_toupper(char *s)
 {
-	int i,x;
-
-	char special[13] = {' ', '\t', '\n', ',', ';', '.',
-		'!', '?', '"', '(', ')', '{', '}'};
+	int i;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
-		s[i] -= 32;
-
-		for (x = 0; x < 13; x++)
-		{
-			if (s[i] = special[x])
-			{
-				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
-					s[i + 1] -= 32;
-			}
-		}
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 32;
 	}
+
 	return (s);
 }
---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+/*
+ * Author: 0xTariq-dev
+ * File: 6-cap_string.c
+ */
+
+#include "main.h"
+
+/**
+ * *cap_string - Capitalize the first letter of every word in string.
+ * @s: The string to be modified.
+ * Return: The resulting string.
+ */
+char *cap_string(char *s)
+{
+	int i, x;
+
+	char special[13] = {' ', '\t', '\n', ',', ';', '.',
+		'!', '?', '"', '(', ')', '{', '}'};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
+
+		for (x = 0; x < 13; x++)
+		{
+			if (s[i] == special[x])
+			{
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+						s[i + 1] -= 32;
+			}
+		}
+	}
+	return (s);
+}
