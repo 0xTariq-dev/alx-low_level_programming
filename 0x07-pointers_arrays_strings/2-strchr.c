35152096149cfb782d987f8724d9d456a2eabf03
0xTariq-dev <tariqmohsen222@gmail.com>
2023-07-03 13:58:49 -0700

    Modified the file 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,7 +8,7 @@
 /**
  * _strchr - Locates a character in a string.
  * @s: The string to be searched.
- * @src: The character to search for.
+ * @c: The character to search for.
  * Return: A pointer to the first occurrence of the char c in
  *	the string s and if not found return null.
  */
@@ -20,8 +20,7 @@ char *_strchr(char *s, char c)
 	{
 		if (s[i] == c)
 		{
-			return s;
-			break;
+			return (s);
 		}
 		i++;
 	}
