b772ff34f4cb8cdacf6938ca1462a10bfaba0826
0xTariq-dev <tariqmohsen222@gmail.com>
2023-07-03 14:46:17 -0700

    Added the file 3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+/*
+ * Author: 0xTariq-dev
+ * File: 3-strspn.c
+ */
+
+#include "main.h"
+
+/**
+ * _strspn - Gets the length of a prefix substring.
+ * @s: The string to be searched for accept bytes.
+ * @accept: The characters to search for.
+ * Return: A number of the initial segment s which
+ *	consist only of bytes drom accept.
+ */
+char *_strchr(char *s, char *accept)
+{
+	int i, x, n;
+
+	f = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (i = 0; s[i] != '\0'; i++)
+		{
+			if (s[i] == accept[j])
+				f++
+		}
+		return (f);
+	}
+	return (0);
+}
