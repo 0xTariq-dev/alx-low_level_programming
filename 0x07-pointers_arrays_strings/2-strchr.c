c8c5930c712998d0423a3a217d4bba7c41dfc0fa
0xTariq-dev <tariqmohsen222@gmail.com>
2023-07-03 14:13:11 -0700

    Modified the file 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -14,16 +14,16 @@
  */
 char *_strchr(char *s, char c)
 {
-	unsigned int i = 0;
+	int i;
 
-	while (s[i] != '\0')
+	while (1)
 	{
-		if (s[i] == c)
-		{
-			return (s);
-		}
-		i++;
+		i = *s++;
+
+		if (i == c)
+			return (s - 1);
+
+		if (i == 0)
+			return (NULL);
 	}
-	if (s[i] == '\0')
-		return (NULL);
 }
