37234a2f172c2a2714abb0cebef476f7a6cb1bf9
0xTariq-dev <tariqmohsen222@gmail.com>
2023-07-09 08:36:18 -0700

    Modified the file 100-cahnge.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,7 +9,7 @@
  *
  * Return: Always 0.
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
 	int i, x, sum = 0;
 
@@ -28,5 +28,5 @@ int main(int argc, char *argv[])
 	}
 
 	printf("%d\n", sum);
-	return (NULL);
+	return (0);
 }
