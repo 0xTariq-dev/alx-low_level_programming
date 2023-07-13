bf037a5c4c07ea6f2c83d2d68fb6e882683ec9bf
0xTariq-dev <tariqmohsen222@gmail.com>
2023-07-12 19:11:53 -0700

    Modified the file 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -36,11 +36,11 @@ int **alloc_grid(int width, int height)
 		{
 			arr[i][x] = 0;
 		}
-		for (i = 0; i < height; i++)
-			free(arr[i]);
-		for (x = 0; x < width; x++)
-			free(arr[x]);
 	}
+	for (i = 0; i < height; i++)
+		free(arr[i]);
+	for (x = 0; x < width; x++)
+		free(arr[x]);
 	return (arr);
 }
 
