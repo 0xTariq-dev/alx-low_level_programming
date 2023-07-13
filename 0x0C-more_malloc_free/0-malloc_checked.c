7f7003d2a57dd48d7943a1856f251ad79f9840b6
0xTariq-dev <tariqmohsen222@gmail.com>
2023-07-13 15:18:02 -0700

    Added the file 0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,24 @@
+/*
+ * Author: 0xTariq-dev
+ * File: 0-malloc_checked.c
+ */
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *malloc_checked - Allocates memory using malloc.
+ * @b: The Size of memory to be allocated.
+ *
+ * Return: A pointer to the allocated memory.
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+	if (b == NULL)
+		exit(98);
+
+	return ();
+}
