d2a535994572b3debd1104283fecf89ec30fb101
0xTariq-dev <tariqmohsen222@gmail.com>
2023-07-12 18:12:02 -0700

    Modified the file 3-str_concat.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -36,13 +36,11 @@ int **alloc_grid(int width, int height)
 		{
 			arr[i][x] = 0;
 		}
-		return (arr);
+		for (i = 0; i < height; i++)
+			free(arr[i]);
+		for (x = 0; x < width; x++)
+			free(arr[x]);
 	}
-	for (i = 0; i < height; i++)
-		free(arr[i]);
-	for (x = 0; x < width; x++)
-		free(arr[x]);
-
-	return (0);
+	return (arr);
 }
 
