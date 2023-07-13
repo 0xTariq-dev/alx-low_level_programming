688a805a9c35c783ee6f4b87cab875e059b01bab
0xTariq-dev <tariqmohsen222@gmail.com>
2023-07-12 17:08:18 -0700

    Modified the file 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -37,12 +37,12 @@ char *str_concat(char *s1, char *s2)
 	if (s1 == NULL)
 	{
 		s1 = '\0';
-		s = s2;
+		*s = s2;
 	}
 	if (s2 == NULL)
 	{
 		s2 = '\0';
-		s = s1;
+		*s = s1;
 	}
 
 	i = _strlen(s1);
