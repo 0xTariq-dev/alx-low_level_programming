37470bfec7ff2fe1d36f4891f9a4503b0349ac13
0xTariq-dev <tariqmohsen222@gmail.com>
2023-07-14 08:48:21 -0700

    Modified the file 2-calloc.c

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,35 @@
+/*
+ * Author: 0xTariq-dev
+ * File: 2-calloc.c
+ */
+
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - Allocates mmory for an array using malloc.
+ * @memb: The number of elements in the array.
+ * @size: The size of the element.
+ *
+ * Return: A pointer to the allocated memory if succes.
+ *	   NULL - if function fails or (size or memb) = 0.
+ */
+
+void *_calloc(unsigned int memb, unsigned int size)
+{
+	char *p;
+	unsigned int n;
+
+	if (size == 0 || memb == 0)
+		return (NULL);
+
+	p = malloc(memb * size);
+
+	if (p == NULL)
+		return (NULL);
+
+	for (n = 0; n < (memb * size); n++)
+		p[n] = 0;
+
+	return (p);
+}
