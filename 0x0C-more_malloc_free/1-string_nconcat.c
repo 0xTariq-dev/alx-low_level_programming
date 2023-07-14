93aa47a6bf8b01284fb55f9ffc016e5e8cca048f
0xTariq-dev <tariqmohsen222@gmail.com>
2023-07-14 07:50:31 -0700

    Modified the file 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,34 +12,37 @@
  * @s2: The second string.
  * @n: The number of string s2 to concatenate to string s2.
  *
- * Return: A pointer to the new string.
+ * Return: A pointer to the new string or NULL if function fails.
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *p;
-	unsigned int size1, size2, i;
+	unsigned int size1 = 0, size2 = 0, i = 0;
 
 	if (s1 == NULL)
 		s1 = "";
 	while (s1[size1])
 		size1++;
-	if (s2 ==NULL)
+
+	if (s2 == NULL)
 		s2 = "";
 	while (s2[size2])
 		size2++;
+
 	if (n >= size2)
 		n = size2;
 
 	p = malloc(size1 + n + 1);
+
 	if (p == NULL)
 		return (NULL);
 
-	for (i = 0; i < (size1 + n); i++)
+	for (; i < (size1 + n); i++)
 	{
 		if (i < size1)
 			p[i] = *s1, s1++;
-		else
+		else 
 			p[i] = *s2, s2++;
 	}
 	p[i] = '\0';
