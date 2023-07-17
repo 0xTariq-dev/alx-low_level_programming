c5851875080c29a1fa4be89aa47cae95c550feef
0xTariq-dev <tariqmohsen222@gmail.com>
2023-07-17 13:28:17 -0700

    Added the file 1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,25 @@
+/*
+ * Author: 0xTariq-dev
+ * File: 0xTariq-dev
+ */
+
+#include "dog.h"
+
+/**
+ * init_dog - Initialize a variable of type struct dog.
+ * @d: The variable to initialize.
+ * @name: The name of the dog.
+ * @age: The dog age.
+ * @owner: The owner name.
+ *
+ * Return: Always 0.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
