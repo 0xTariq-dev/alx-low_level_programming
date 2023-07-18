d28d884208b73ed107fc5da6508edf3086fe2821
0xTariq-dev <tariqmohsen222@gmail.com>
2023-07-17 19:57:37 -0700

    Added the file 2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,18 @@
+/*
+ * Author: 0xTariq-dev
+ * File: 2-main.c
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the file.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
