95da7e13944ac44a20fe4f0a89108b9ce93bcc35
0xTariq-dev <tariqmohsen222@gmail.com>
2023-07-20 19:20:25 -0700

    Modified the file 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -8,7 +8,7 @@
 #include <stdio.h>
 
 /**
- * sprint_numbers - prints all of it's parameters.
+ * print_numbers - prints all of it's parameters.
  * @separator: The string to print between numbers.
  * @n: The number of parameters.
  * @...: The other parameters to print.
@@ -18,19 +18,20 @@
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list numbers;
-	unsigned int i = 0, x = 0, num;
+	unsigned int i = 0;
+	int num;
 
 	va_start(numbers, n);
 
 	for (; i < n; i++)
 	{
-		x = va_arg(numbers, int);
-		num = x;
+		num = va_arg(numbers, int);
+		printf("%d", num);
 		if (separator == NULL)
-			printf("%d", num);
-		else
-			printf("%d%s", num, separator);
+			continue;
+		if (i < n - i)
+			printf("%s", separator);
 	}
-	va_end(numbers);
 	printf("\n");
+	va_end(numbers);
 }
