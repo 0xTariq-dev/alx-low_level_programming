af38e183fec6ceb460668e9a24feb30613d93bdb
0xTariq-dev <tariqmohsen222@gmail.com>
2023-07-20 19:11:55 -0700

    Modified the file 1-rint_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -20,16 +20,16 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list numbers;
 	unsigned int i = 0, x = 0, num;
 
-	if (separator == NULL)
-		separator = "";
-
 	va_start(numbers, n);
 
 	for (; i < n; i++)
 	{
 		x = va_arg(numbers, int);
 		num = x;
-		printf("%d%s", num, separator);
+		if (separator == NULL)
+			printf("%d", num);
+		else
+			printf("%d%s", num, separator);
 	}
 	va_end(numbers);
 	printf("\n");
