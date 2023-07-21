5cabcf14661375a059a69f2706a419e548e13a25
0xTariq-dev <tariqmohsen222@gmail.com>
2023-07-20 19:01:49 -0700

    fixed a betty error in the header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,7 +6,7 @@
  * file: main.h
  * author: 0xTariq-dev
  * describtion: Header file contains declarations for functions
- * 		used in 0x10-variadic_functions project
+ *		used in 0x10-variadic_functions project
  */
 
 int _putchar(char c);
