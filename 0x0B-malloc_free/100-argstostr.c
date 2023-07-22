fef08cfe30b6813e39c044eb87c14a3cad8a8d1c
0xTariq-dev <tariqmohsen222@gmail.com>
2023-07-21 17:39:03 -0700

    added the file 100-agrstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -32,7 +32,7 @@ char *argstostr(int ac, char **av)
 		size++;
 		i++;
 	}
-	arg = malloc(sizeof(char) * size);
+	arg = malloc((sizeof(char) * size) - 2);
 	if (arg == NULL)
 		return (NULL);
 	i = 0;
