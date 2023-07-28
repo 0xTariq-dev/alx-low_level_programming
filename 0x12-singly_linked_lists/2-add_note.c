b8dbfa0bf72df19a5db0063fef389d006e0e68f1
0xTariq-dev <tariqmohsen222@gmail.com>
2023-07-27 19:51:10 -0700

    Added the file 2-add_node.c

---
+++ 0x12-singly_linked_lists/2-add_note.c
@@ -0,0 +1,35 @@
+/*
+ * Author: 0xTariq-dev
+ * File: 1-add_note.c
+ */
+
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * add_note - Add a new node at the start of a linked list.
+ * @head: A double pointer to the head of the linked list.
+ * @str: The string to be added to the new nodes
+ * Return: The Address of the element or NULL if it fails.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	unsigned int count = 0;
+
+	while (str[count])
+		count++;
+
+	new = malloc(sizeof(list_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	new->str = strdup(str);
+	new->len = count;
+	new->next = *head;
+	*head = new;
+
+	return (*head);
+}
