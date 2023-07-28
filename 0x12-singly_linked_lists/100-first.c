52b7f6d9669395c0ab923412ec4db076c4dbc2ac
0xTariq-dev <tariqmohsen222@gmail.com>
2023-07-28 12:32:56 -0700

    Fixed a betty error in the file 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,11 +5,11 @@
 
 #include <stdio.h>
 
-void first(void) __attribute__((constructor));
+void print_first(void) __attribute__((constructor));
 /**
  * print_first - Prints a string pefore the main function execution.
  */
-void first(void)
+void print_first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
