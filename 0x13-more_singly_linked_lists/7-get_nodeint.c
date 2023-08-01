efa6dab218abe64dd5423da8f5e6eaaa7b82b4ef
0xTariq-dev <tariqmohsen222@gmail.com>
2023-08-01 16:14:49 -0700

    Added the file 7-get_nodeint_ondex.c

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,30 @@
+/*
+ * Author: 0xTariq-dev
+ * File: 6-pop_listint.c
+ */
+
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * get_nodeint_index - Gets a specific node of a list with it's index.
+ * @head: A pointer to the linked list to be freed.
+ * @index: The index of the node to get.
+ *
+ * Return: The head node data or 0 if te list is empty.
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int x = 0;
+
+	if (head == NULL)
+		return (NULL);
+	while (head)
+	{
+		if (x = index)
+			return (head);
+		head = (*head)->next;
+		i++;
+	}
+	return (NULL);
+}
