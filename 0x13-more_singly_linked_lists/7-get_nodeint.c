6f0c0487ef3693803b082e8316a8e319c18f5e78
0xTariq-dev <tariqmohsen222@gmail.com>
2023-08-01 16:20:29 -0700

    Added the file 7-get_nodeint_ondex.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -17,14 +17,12 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int x = 0;
 
-	if (head == NULL)
-		return (NULL);
-	while (head)
+	for (; x < index; x++)
 	{
-		if (x == index)
-			return (head);
+		if (head == NULL)
+			return (NULL);
+
 		head = head->next;
-		i++;
 	}
-	return (NULL);
+	return (head);
 }
