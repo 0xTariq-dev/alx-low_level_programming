b18d2cd0b162f94764b5f3e333fd7484df68a9c7
0xTariq-dev <tariqmohsen222@gmail.com>
2023-08-06 00:20:26 -0700

    Added the file 100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,23 @@
+/*
+ * Author: 0xTariq-dev
+ * File: 100-get_endianness.c
+ */
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * get_endianess - Checks if a number is a little endian (reversed order to
+ *	human readable order) or big endian (ordered as human readable order).
+ *
+ * Return: 0 if big endian or 1 if little endian,
+ */
+int get_endianness(void)
+{
+	int x = 1;
+	char *p = (char*)&x;
+
+	if (*p == 1)
+		return (1);
+	return (0);
+}
