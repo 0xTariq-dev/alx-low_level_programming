28e5cc9ca76c702a5f1f253d41f7372aef384e6f
0xTariq-dev <tariqmohsen222@gmail.com>
2023-08-07 18:40:05 -0700

    Modified the file 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,7 +26,7 @@ int create_file(const char *filename, char *text_content)
 		for (; text_content[i]; i++)
 			;
 	}
-	op = open(filename, O_CREAT | O_RDWR | O_TRUNC);
+	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	cont = write(op, text_content, i);
 
 	if (op == -1 || cont == -1)
