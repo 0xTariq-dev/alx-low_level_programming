b69a5bbabc4555f2b3d4108bc7ae8fe0a8ff39f1
0xTariq-dev <tariqmohsen222@gmail.com>
2023-09-14 08:55:41 -0700

    fix: Output fail on printing list elements

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -16,6 +16,8 @@ size_t print_dlistint(const dlistint_t *h)
 	dlistint_t *current = (dlistint_t *)h;
 	size_t x = 0;
 
+	if (current == NULL)
+		return (0);
 	while (current != NULL)
 	{
 		printf("%d \n", current->n);
