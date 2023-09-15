2313e85484843a90e552b58f2159f3af1f107007
0xTariq-dev <tariqmohsen222@gmail.com>
2023-09-15 08:44:55 -0700

    feat: Insert node at specifec index

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -20,11 +20,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 		return (NULL);
 
 	for (; x < index; x++)
-	{
-		if (head == NULL)
-			return (NULL);
 		head = head->next;
-	}
 
 	return (head);
 }
---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,43 @@
+/*
+ * Author: 0xTariq-dev
+ * File: 7-insert_dnodeint_at_index.c
+ */
+
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - insert a node at specifec index of a list.
+ * @h: A pointer to the list.
+ * @idx: The index to insert the node at.
+ * @n: The data of the new node.
+ *
+ * Return: The node address.
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new, *tmp = *h;
+	unsigned int x = 0;
+
+	if ((new = malloc(sizeof(dlistint_t))) == NULL)
+		return (NULL);
+	new->n = n;
+
+	if (*h == NULL || idx == 0)
+		new = add_dnodeint(h, n);
+
+	for (; x < idx; x++)
+	{
+		if (tmp == NULL)
+		{
+			free(new);
+			return (NULL);
+		}
+		tmp = tmp->next;
+	}
+
+	new->prev = tmp->prev;
+	new->next = tmp;
+	tmp->prev->next = new;
+	tmp->prev = new;
+	return (new);
+}
