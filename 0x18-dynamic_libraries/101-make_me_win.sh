eb8455614044d9fd56bc4f6a286ef34ae19e2d7e
0xTariq-dev <tariqmohsen222@gmail.com>
2023-09-19 01:51:22 -0700

    fix: Exporting the library from tmp directory

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -P /tmp https://github.com/0xTariq-dev/alx-low_level_programming/blob/0xTariq-dev/0x18-dynamic_libraries/npick.so
-export LD_PRELOAD=/tmp/npick.so
+export LD_PRELOAD=/./npick.so
