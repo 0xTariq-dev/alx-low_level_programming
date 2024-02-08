ec722bc2a807f8393c6908865f0aebc4e48ff31d
Tariq Abu-Elhamd <tariqmohsen222@gmail.com>
2024-02-08 16:51:21 +0200

    fix: Advanced binary search index return value.

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,16 +1,16 @@
 #include "search_algos.h"
 
 /**
- * advanced_binary - searches for a value in a sorted array of integers.
- * @array: pointer to the first element of the array to search in.
- * @size: number of elements in array.
- * @value: value to search for.
- * Return: The first index where value is located
- *			or -1 if value is not present.
+* advanced_binary - searches for a value in a sorted array of integers.
+* @array: pointer to the first element of the array to search in.
+* @size: number of elements in array.
+* @value: value to search for.
+* Return: The first index where value is located
+*			or -1 if value is not present.
 */
 int advanced_binary(int *array, size_t size, int value)
 {
-	size_t start = 0, end = size - 1, mid, i, j;
+	size_t start = 0, end = size - 1, mid, i;
 
 	if (array == NULL)
 		return (-1);
@@ -23,15 +23,9 @@ int advanced_binary(int *array, size_t size, int value)
 		printf("%d\n", array[i]);
 
 		mid = (start + end) / 2;
-		j = mid - 1;
 		if (array[mid] == value)
-		{
-			while (array[j] == value && j != 0)
-			{
-				return (j);
-			}
-			return (mid);
-		}
+			return (array[mid - 1] == value ?
+				(size_t)advanced_binary(array, mid + 1, value) : mid);
 		else if (array[mid] > value)
 			end = mid - 1;
 		else
